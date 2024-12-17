0d4e17cad48a45c3fa102dc627b75e9e3defbe4a
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-18 00:11:19 +0800

    made loader simpler

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,29 +1,10 @@
 import React from 'react'
-import { inView, motion } from 'framer-motion'
-import { TextShimmer } from '@/components/ui/text-shimmer'
 
 export default function Loader({ message }: { message?: string }) {
     return (
-        <div>
-            <div className="flex items-center justify-center space-x-1">
-                {[...Array(5)].map((_, index) => (
-                    <motion.div
-                        key={index}
-                        className="h-8 w-2 rounded bg-muted-foreground/70"
-                        animate={{
-                            scaleY: [1, 1.5, 1],
-                            translateY: ['0%', '-25%', '0%'],
-                        }}
-                        transition={{
-                            duration: 1,
-                            repeat: Infinity,
-                            ease: 'easeInOut',
-                            delay: index * 0.1,
-                        }}
-                    />
-                ))}
-            </div>
-            {message && <TextShimmer className='mt-2 font-medium text-2xl'>{message}</TextShimmer>}
+        <div className="flex flex-col items-center gap-2">
+            <div className="h-10 w-10 animate-spin rounded-full border-4 border-gray-200 border-t-primary" />
+            {message && <span className='mt-2 font-medium text-2xl'>{message}</span>}
         </div>
     )
 }
