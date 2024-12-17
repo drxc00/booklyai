8d31d13c73bbf144e95857fd9f9c011c437042f5
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-18 00:11:39 +0800

    added suppress hydration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
 }>) {
 
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <ReactQueryProvider>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased m-0 p-0 w-full h-full`}
