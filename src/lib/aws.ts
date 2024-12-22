529615d4c58bc6de614b300e2f2fca2a33c6b1c9
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-22 18:09:03 +0800

    feat: added s3 Request presigner

--- src/lib/aws.ts
+++ src/lib/aws.ts
@@ -1,5 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import { S3RequestPresigner } from "@aws-sdk/s3-request-presigner";
 import { loadEnvConfig } from "@next/env";
+import { Hash } from "@aws-sdk/hash-node";
 
 loadEnvConfig(process.cwd()); // Ensure that the .env file is loaded
 
@@ -12,6 +14,7 @@ export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_KEY
 // Create an instance of the S3Client
 
 let s3Client: S3Client;
+let s3RequestPresigner: S3RequestPresigner;
 
 export const getS3Client = () => {
     if (!s3Client) {
@@ -26,3 +29,18 @@ export const getS3Client = () => {
     }
     return s3Client;
 };
+
+export const getS3RequestPresigner = () => {
+    if (!s3RequestPresigner) {
+        s3RequestPresigner = new S3RequestPresigner({
+            region: REGION as string,
+            credentials: {
+                accessKeyId: AWS_ACCESS_KEY_ID as string,
+                secretAccessKey: AWS_SECRET_ACCESS_KEY as string,
+            },
+            sha256: Hash.bind(null, "sha256")
+        });
+        console.log('S3 Request Presigner created');
+    }
+    return s3RequestPresigner;
+}
