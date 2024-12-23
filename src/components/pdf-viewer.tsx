ae873170d49b3bacb29fd401a66eb4510d4175df
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-23 17:48:50 +0800

    pref: made the book preview simpler

--- src/components/pdf-viewer.tsx
+++ src/components/pdf-viewer.tsx
@@ -1,30 +1,11 @@
-"use client";
 
-import React, { useCallback, useEffect, useState } from 'react';
-const PDFViewer = ({ bookId, type }: { bookId: string, type: string }) => {
-
-    const [pdfUrl, setPdfUrl] = useState<string | null>(null);
-
-    useEffect(() => {
-        // Fetch the PDF URL from the server
-        fetch(`/api/get-pdf?key=${bookId}&type=${type}`)
-            .then(response => response.json())
-            .then(data => setPdfUrl(data.url))
-            .catch(error => console.error('Error fetching PDF URL:', error));
-    }, []);
-
-
+const PDFViewer = ({ url }: { url: string }) => {
     return (
         <div className="mt-8 flex flex-col items-center justify-center w-full">
             <div className="w-full border p-2 bg-muted rounded-md mb-4 flex gap-2 items-center justify-center">
-                {pdfUrl ? (
-                    <embed src={pdfUrl} type="application/pdf" width="100%" height="600px" />
-                ) : (
-                    <div>Loading PDF...</div>
-                )}
+                <embed src={url as string} type="application/pdf" width="100%" height="600px" />
             </div>
         </div>
     );
 };
-
 export default PDFViewer;
