1cd6980daaf7565495a97fca25c8c7110e31a5f1
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-23 14:49:59 +0800

    ref: prepare config for self-hosting

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  serverExternalPackages: ["@react-pdf/renderer"],
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config
-  }
+  output: "standalone" 
 };
 
 export default nextConfig;
