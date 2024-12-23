738d066afa9deee4783724601cc2560020d656b2
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-23 15:01:35 +0800

    ref: update conditional render

--- src/components/book-list.tsx
+++ src/components/book-list.tsx
@@ -29,22 +29,25 @@ const BookCard = ({ book, className }: { book: BookDocument, className?: string 
 export default function BookList({ books, direction }: BookListProps) {
     // The direction prop is passed from the page (server-side) and used to decide layout
 
-    return (
-        <div className=" items-center">
-            {/* Conditionally render based on direction */}
-            {direction === "horizontal" ? (
-                <InfiniteSlider direction="horizontal" className="flex gap-4 whitespace-nowrap">
-                    {books.map((book) => (
-                        <BookCard key={book.id} book={book} />
-                    ))}
-                </InfiniteSlider>
-            ) : (
-                <InfiniteSlider direction="vertical" className="flex flex-col gap-4 max-w-full lg:max-h-lvh">
+    if (direction == "vertical") {
+        return (
+            <div className=" items-center">
+                <InfiniteSlider direction="vertical" className="flex flex-col gap-4 max-w-full lg:max-h-lvh" key="vertical-slider">
                     {books.map((book) => (
                         <BookCard key={book.id} book={book} className="w-full max-w-lg" />
                     ))}
                 </InfiniteSlider>
-            )}
+            </div>
+        )
+    }
+
+    return (
+        <div className=" items-center">
+            <InfiniteSlider direction="horizontal" className="flex gap-4 whitespace-nowrap" key="horizontal-slider">
+                {books.map((book) => (
+                    <BookCard key={book.id} book={book} />
+                ))}
+            </InfiniteSlider>
         </div>
     );
 }
