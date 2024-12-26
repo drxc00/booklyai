bf1e0b799ddae212e75f6b6fc9a76cd244b5a93c
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-26 22:52:28 +0800

    ref: fixed redirect when not authenticated

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,17 +1,13 @@
 import NavBar from "@/components/nav-bar";
 import cachedSession from "@/lib/session-cache";
+import { redirect } from "next/navigation";
 
 export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
     // Session manager for all routes
     const session = await cachedSession();
-    if (!session) {
-        return {
-            redirect: {
-                destination: "/login", // Redirect to login page if no session
-                permanent: false, // Temporary redirect
-            },
-        };
-    }
+    
+    if (!session) redirect("/login");
+
     return (
         <div className="">
             {children}
