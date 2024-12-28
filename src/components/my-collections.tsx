00655b6cd3b21aa0c334a97950b0caa6f0250d9c
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-28 13:16:39 +0800

    style: updated card style

--- src/components/my-collections.tsx
+++ src/components/my-collections.tsx
@@ -11,11 +11,11 @@ export default function MyCollections({ books }: { books: BookDocument[] }) {
             {books.map((book) => (
                 <Card
                     key={book.id}
-                    className="w-full bg-background shadow-none cursor-pointer">
+                    className="w-full bg-card shadow-none cursor-pointer">
                     <CardContent className="pt-6 h-full flex flex-col justify-between gap-3">
                         <h3 className="font-semibold text-lg mb-2 line-clamp-2 font-serif">{book.title}</h3>
-                        <div className="flex items-center gap-1">
-                            <p className="text-sm text-foreground flex items-center">{book.isPurchased ? (
+                        <div className="flex items-center gap-1 text-muted-foreground">
+                            <p className="text-sm flex items-center">{book.isPurchased ? (
                                 <span className="flex items-center gap-2">
                                     <CheckCircle size={15} />
                                     <span>Purchased</span>
@@ -26,21 +26,22 @@ export default function MyCollections({ books }: { books: BookDocument[] }) {
                                     <span>Not Purchased</span>
                                 </span>
                             )}</p>
-                            <Dot className="text-muted-foreground" />
-                            <p className="text-sm text-foreground">{generatedSinceWhen(new Date(book.createdAt as Date))} ago</p>
+                            <Dot />
+                            <p className="text-sm">{generatedSinceWhen(new Date(book.createdAt as Date))} ago</p>
 
                         </div>
 
                         <Link href={book.isPurchased ? `/dashboard/book/${book.id}` : `/dashboard/generate/${book.id}/preview`}>
-                            <Button variant={book.isPurchased ? "default" : "outline"} className="w-full">
+                            <Button variant={book.isPurchased ? "default" : "outline"} 
+                                className="w-full bg-secondary-foreground hover:bg-secondary/5">
                                 {book.isPurchased ? (
                                     <>
-                                        <Download className="w-4 h-4 mr-2" />
+                                        <Download className="w-4 h-4" />
                                         Download
                                     </>
                                 ) : (
                                     <>
-                                        <Eye className="w-4 h-4 mr-2" />
+                                        <Eye className="w-4 h-4" />
                                         Preview
                                     </>
                                 )}
