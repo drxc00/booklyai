36eebf8cab70aca6c0524b9c052e7b9af5e225d7
Neil Patrick Villanueva <neilpatz16@gmail.com>
2024-12-29 18:47:08 +0800

    feat: added more auth check

---
+++ src/app/dashboard/book/layout.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import getCachedSession from "@/lib/session-cache";
+import { redirect } from "next/navigation";
+export default async function Layout({
+    children
+}: {
+    children: React.ReactNode, params: Promise<{ bookId: string }>
+}) {
+
+    const session = await getCachedSession();
+    if (!session) redirect("/login");
+
+    return (
+        <>
+            {children}
+        </>
+    )
+}
